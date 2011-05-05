6941608e83554fbc4b2e92137bef6dcf3249a75c
Marc Brinkmann <git@marcbrinkmann.de>
2011-05-05 11:45:13 +0200

    PEP8.

--- gomcmd.py
+++ gomcmd.py
@@ -28,69 +28,77 @@ import re
 import urllib
 from xml.dom import minidom
 
-class ProgException(Exception): pass
+
+class ProgException(Exception):
+    pass
 
 
-def register_gnome_url_handler(urltype, command, enabled = True, needs_terminal = False):
-	import gconf # not everyone has gconf, small non-critical performance hit importing
-	             # it here!
+def register_gnome_url_handler(urltype,
+                               command,
+                               enabled=True,
+                               needs_terminal=False):
+    import gconf
 
-	dirname = '/desktop/gnome/url-handlers/%s' % urltype
-	c = gconf.client_get_default()
+    dirname = '/desktop/gnome/url-handlers/%s' % urltype
+    c = gconf.client_get_default()
 
-	# create the directory
-	if not c.dir_exists(dirname):
-		print "created %s" % dirname
-		c.add_dir(dirname, gconf.CLIENT_PRELOAD_NONE)
+    # create the directory
+    if not c.dir_exists(dirname):
+        print "created %s" % dirname
+        c.add_dir(dirname, gconf.CLIENT_PRELOAD_NONE)
 
-	# create the values
-	v = gconf.Value(gconf.VALUE_STRING)
-	v.set_string(command)
-	c.set(dirname + '/command', v)
+    # create the values
+    v = gconf.Value(gconf.VALUE_STRING)
+    v.set_string(command)
+    c.set(dirname + '/command', v)
 
-	v = gconf.Value(gconf.VALUE_BOOL)
-	v.set_bool(enabled)
-	c.set(dirname + '/enabled', v)
+    v = gconf.Value(gconf.VALUE_BOOL)
+    v.set_bool(enabled)
+    c.set(dirname + '/enabled', v)
 
-	v = gconf.Value(gconf.VALUE_BOOL)
-	v.set_bool(needs_terminal)
-	c.set(dirname + '/needs_terminal', v)
+    v = gconf.Value(gconf.VALUE_BOOL)
+    v.set_bool(needs_terminal)
+    c.set(dirname + '/needs_terminal', v)
 
 
 def parse_url(url):
-	m = re.match('^.*(http://.*)$', url)
-	if not m:
-		raise ProgException('Not in URL format: %s' % url)
-	data = urllib.urlopen(m.group(1)).read()
-	doc = minidom.parseString(data)
-	ref = doc.getElementsByTagName('REF')[0]
-	return ref.getAttribute('href')
+    m = re.match('^.*(http://.*)$', url)
+    if not m:
+        raise ProgException('Not in URL format: %s' % url)
+    data = urllib.urlopen(m.group(1)).read()
+    doc = minidom.parseString(data)
+    ref = doc.getElementsByTagName('REF')[0]
+    return ref.getAttribute('href')
 
 
 try:
-	parser = optparse.OptionParser()
-	parser.add_option('-M','--media-player',default='vlc',help='the media player to use. defaults to VLC')
-	parser.add_option('-G','--register-gnome-handlers',default=False,action='store_true',help='register url handlers with gnome')
+    parser = optparse.OptionParser()
+    parser.add_option('-M', '--media-player', default='vlc',
+                      help='the media player to use. defaults to VLC')
+    parser.add_option('-G', '--register-gnome-handlers',
+                      default=False, action='store_true',
+                      help='register url handlers with gnome')
 
-	opts, args = parser.parse_args()
+    opts, args = parser.parse_args()
 
-	# register url handlers?
-	if opts.register_gnome_handlers:
-		import gconf
-		register_gnome_url_handler('gomcmd', "%s '%%s'" % os.path.abspath(sys.argv[0]))
-		# do nothing else
-		sys.exit(0)
+    # register url handlers?
+    if opts.register_gnome_handlers:
+        import gconf
+        register_gnome_url_handler('gomcmd', "%s '%%s'" %
+                                   os.path.abspath(sys.argv[0]))
+        # do nothing else
+        sys.exit(0)
 
-	if not args:
-		raise ProgException('No arguments')
-	if 1 != len(args):
-		raise ProgException('Too many arguments')
+    if not args:
+        raise ProgException('No arguments')
+    if 1 != len(args):
+        raise ProgException('Too many arguments')
 
-	# convert the argument
-	url = parse_url(args[0])
+    # convert the argument
+    url = parse_url(args[0])
 
-	print "opening %s %s" % (opts.media_player, url)
+    print "opening %s %s" % (opts.media_player, url)
 
-	os.execlp(opts.media_player, opts.media_player, url)
+    os.execlp(opts.media_player, opts.media_player, url)
 except ProgException, e:
-	sys.stderr.write('%s: %s%s' % (parser.get_prog_name(), e, os.linesep))
+    sys.stderr.write('%s: %s%s' % (parser.get_prog_name(), e, os.linesep))
